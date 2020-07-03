c50b94e5adfb5a96732ba897b4451e3e76081624
DutcherNL <wouter@fam-roos.com>
2020-07-03 17:22:27 +0200

    Links in text module now open in a new tab

--- PageDisplay/templatetags/full_domain_urls.py
+++ PageDisplay/templatetags/full_domain_urls.py
@@ -1,4 +1,5 @@
 from django import template
+from django.utils.safestring import mark_safe
 
 from general import get_absolute_url_path
 
@@ -9,3 +10,12 @@ register = template.Library()
 @register.filter
 def absolute_url(path):
     return get_absolute_url_path(path)
+
+
+@register.filter
+def urls_in_new_tab(string):
+    """ Adds the target to a new tab for any link encountered (requires <a> tags """
+    return mark_safe(string.replace(
+        '<a',
+        '<a target="_blank" '
+    ))
