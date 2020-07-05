3757688ec55fd80e2db534d251b4834d768e418c
wouter@fam-roos.com <Knights4Life>
2020-07-06 00:33:23 +0200

    WIP Question modules

--- Questionaire/modules/widgets.py
+++ Questionaire/modules/widgets.py
@@ -1,6 +1,26 @@
 from PageDisplay.module_widgets import BaseModuleWidget
 
 from Questionaire.models import Inquirer
+from Questionaire.fields import QuestionFieldFactory
+
+
+class QuestionWidget(BaseModuleWidget):
+    template_name = "inquiry/modules/module_question.html"
+    use_from_context = ['inquirer', 'form']
+
+    def get_context_data(self, request=None, inquirer=None, **kwargs):
+        if inquirer:
+            inquiry = inquirer.active_inquiry
+        else:
+            inquiry = None
+
+        context = super(QuestionWidget, self).get_context_data(request, **kwargs)
+        context['field'] = QuestionFieldFactory.get_field_by_questionmodel(
+            question=self.model.question,
+            inquiry=inquiry,
+            required=self.model.required,
+        )
+        return context
 
 
 class TechScoreWidget(BaseModuleWidget):
