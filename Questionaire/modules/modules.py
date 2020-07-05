71b356b26ab8d6f22573a5b9c707770f37cd4c9d
wouter@fam-roos.com <Knights4Life>
2020-07-06 00:33:23 +0200

    Implemented question module

--- Questionaire/modules/modules.py
+++ Questionaire/modules/modules.py
@@ -3,8 +3,17 @@ from django.db import models
 from PageDisplay.models import BaseModule, BasicModuleMixin
 from PageDisplay.module_registry import registry
 
-from Questionaire.models import Technology
-from Questionaire.modules.widgets import TechScoreWidget
+from Questionaire.models import Technology, Question
+from Questionaire.modules.widgets import *
+
+
+class QuestionModule(BasicModuleMixin, BaseModule):
+    _type_id = 17
+    verbose = "Question"
+    widget = QuestionWidget
+
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    required = models.BooleanField(default=False)
 
 
 class TechScoreModule(BasicModuleMixin, BaseModule):
@@ -21,3 +30,4 @@ class TechScoreModule(BasicModuleMixin, BaseModule):
 
 
 registry.register(TechScoreModule)
+registry.register(QuestionModule)
--- Questionaire/modules/widgets.py
+++ Questionaire/modules/widgets.py
@@ -1,6 +1,40 @@
+from django.forms import Form
+
 from PageDisplay.module_widgets import BaseModuleWidget
 
 from Questionaire.models import Inquirer
+from Questionaire.fields import QuestionFieldFactory
+
+
+class QuestionWidget(BaseModuleWidget):
+    template_name = "inquiry/modules/module_question.html"
+    use_from_context = ['inquirer', 'inquiry_form']
+
+    def get_context_data(self, request=None, inquirer=None, inquiry_form=None, **kwargs):
+        if inquirer:
+            inquiry = inquirer.active_inquiry
+        else:
+            inquiry = None
+
+        if inquiry_form:
+            field = inquiry_form[self.model.question.name]
+        else:
+            # The field is not in a form context (e.g. on an edit page)
+            # create a simple form to contain the field in instead (this prevents any errors due to field rendering
+            # without any form related attributes
+            class SimpleMockForm(Form):
+                field = QuestionFieldFactory.get_field_by_questionmodel(
+                    question=self.model.question,
+                    inquiry=inquiry,
+                    required=self.model.required,
+                )
+            field = SimpleMockForm()['field']
+
+        return super(QuestionWidget, self).get_context_data(
+            request,
+            field=field,
+            **kwargs
+        )
 
 
 class TechScoreWidget(BaseModuleWidget):
