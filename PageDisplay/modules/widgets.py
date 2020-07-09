6560ad16c594a704dc82d3aa6f9762dff8bc3212
DutcherNL <wouter@fam-roos.com>
2020-07-09 16:52:17 +0200

    Fixed capital lettering link error undetected by windows

--- PageDisplay/modules/widgets.py
+++ PageDisplay/modules/widgets.py
@@ -2,4 +2,4 @@ from PageDisplay.module_widgets import BaseModuleWidget
 
 
 class DownloadFileWidget(BaseModuleWidget):
-    template_name = "PageDisplay/modules/module_download_file.html"
+    template_name = "pagedisplay/modules/module_download_file.html"
