80bcf5a2f4d49faf853ab9bcfb236a75eb11cdfb
DutcherNL <wouter@fam-roos.com>
2020-07-17 17:50:12 +0200

    Small layout tweaks in user settings

--- inquirer_settings/urls.py
+++ inquirer_settings/urls.py
@@ -6,7 +6,7 @@ app_name = "inquirer_settings"
 
 urlpatterns = [
     path('settings/', include([
-        path('home/', views.InquirerSettingsHome.as_view(), name='home'),
+        path('', views.InquirerSettingsHome.as_view(), name='home'),
         path('mail/', views.InquirerMailSettingsView.as_view(), name='mail'),
         path('interests/', views.CollectiveInterestView.as_view(), name='tech_cols'),
         path('verzamelde_gegevens/', views.InquirerAnswersView.as_view(), name='answers'),
