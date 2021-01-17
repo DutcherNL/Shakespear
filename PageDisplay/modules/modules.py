5668e78a5f9888aefe8d09777369282aa4c489f1
DutcherNL <wouter@fam-roos.com>
2021-01-17 16:49:07 +0100

    Added ExternalURL module

--- PageDisplay/modules/modules.py
+++ PageDisplay/modules/modules.py
@@ -1,9 +1,12 @@
 from django.db import models
+from django.core.validators import URLValidator
+from django.core.exceptions import ValidationError
+from django.utils.translation import gettext_lazy as _
 
 from PageDisplay.models import BaseModule, BasicModuleMixin
 from PageDisplay.module_registry import registry
 
-from .widgets import DownloadFileWidget
+from .widgets import DownloadFileWidget, ExternalURLWidget
 
 
 class DownloadFileModule(BasicModuleMixin, BaseModule):
@@ -15,4 +18,18 @@ class DownloadFileModule(BasicModuleMixin, BaseModule):
     description = models.CharField(max_length=512)
 
 
+def validate_test(value):
+    raise ValidationError("Validation halted")
+
+
+class ExternalURLModule(BasicModuleMixin, BaseModule):
+    _type_id = 22
+    verbose = "Drop URL"
+    widget = ExternalURLWidget
+
+    url = models.CharField(max_length=128, validators=[URLValidator()])
+    guide_text = models.CharField(max_length=128, default=_('View online'))
+
+
 registry.register(DownloadFileModule)
+registry.register(ExternalURLModule)
--- PageDisplay/modules/widgets.py
+++ PageDisplay/modules/widgets.py
@@ -3,3 +3,7 @@ from PageDisplay.module_widgets import BaseModuleWidget
 
 class DownloadFileWidget(BaseModuleWidget):
     template_name = "pagedisplay/modules/module_download_file.html"
+
+
+class ExternalURLWidget(BaseModuleWidget):
+    template_name = "pagedisplay/modules/module_external_uri.html"
