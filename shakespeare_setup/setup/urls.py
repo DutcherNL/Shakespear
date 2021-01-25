5acf228b83e43934f78adfcc280b5208d52e2802
DutcherNL <wouter@fam-roos.com>
2021-01-25 13:44:19 +0100

    Adjusted local datastorage setup urls for use of new setup system

--- shakespeare_setup/setup/urls.py
+++ shakespeare_setup/setup/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('', SetUpOverview.as_view(), name='home'),
     path('reports/', include('reports.urls')),
     # path('mailings/', include('questionaire_mailing.urls')),
-    path('data_storage/', include('local_data_storage.urls')),
+    # path('data_storage/', include('local_data_storage.urls')),
     path('queued_tasks/', include('queued_tasks.urls')),
 
 ]
