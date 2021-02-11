86bb3fdfaaa217d4bd85d9fa03d28b212d59fc93
DutcherNL <wouter@fam-roos.com>
2021-02-11 17:32:21 +0100

    Tweaked image in mail to be absolute instead of local (oops)

--- general/templatetags/django_extension_tags.py
+++ general/templatetags/django_extension_tags.py
@@ -1,6 +1,9 @@
 from django import template
 from django.template.defaultfilters import urlize
 from django.utils.safestring import mark_safe
+from django.templatetags.static import StaticNode
+
+from general import get_absolute_url_path
 
 register = template.Library()
 
@@ -12,3 +15,18 @@ def urlize_new_tab(string):
         '<a',
         '<a target="_blank" '
     ))
+
+
+class AbsStaticNode(StaticNode):
+    """ A tweak on the static node that instead returns absolute urls"""
+    def url(self, context):
+        path = super(AbsStaticNode, self).url(context)
+        return get_absolute_url_path(path)
+
+
+@register.tag('static_abs')
+def do_static_abs(parser, token):
+    """
+    Similar to static tags, except it returns an absolute path instead
+    """
+    return AbsStaticNode.handle_token(parser, token)
