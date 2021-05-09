3d199c90ff4eee89a6d34002874eb6fa896aaf60
DutcherNL <wouter@fam-roos.com>
2021-05-09 17:20:38 +0200

    Kleine verbetering stap 3 weergave

--- initiative_enabler/admin.py
+++ initiative_enabler/admin.py
@@ -20,41 +20,42 @@ class TechCollectiveAdmin(admin.ModelAdmin):
     inlines = [RestrictionLinkInlines]
 
 
-@admin.register(TechCollectiveInterest)
-class TechCollectiveInterestAdmin(admin.ModelAdmin):
-    fields = ('inquirer', 'tech_collective', 'is_interested')
-
-    list_display = ('__str__', 'inquirer', 'is_interested')
-    list_filter = ('inquirer',)
-
-    class RestrictionsInlines(admin.TabularInline):
-        model = TechCollectiveInterest.restriction_scopes.through
-        fields = ('restriction', 'value')
-        readonly_fields = ('restriction', 'value')
-        extra = 0
-
-        def has_add_permission(self, request, obj):
-            # This should only display info, not allow changing
-            return False
-
-        def restriction(self, instance):
-            return instance.restrictionvalue.restriction.name
-        restriction.short_description = 'restriction name'
-
-        def value(self, instance):
-            return instance.restrictionvalue.value
-        value.short_description = 'local value'
-
-    inlines = [RestrictionsInlines]
-
-
 # Register your models here.
 admin.site.register(RestrictionRangeAdjustment)
-admin.site.register(RestrictionValue)
 admin.site.register(TechImprovement)
+admin.site.register(CollectiveQuestionRestriction)
+
 
 if settings.SHOW_DEBUG_CLASSES:
-    admin.site.register(CollectiveQuestionRestriction)
+
+    @admin.register(TechCollectiveInterest)
+    class TechCollectiveInterestAdmin(admin.ModelAdmin):
+        fields = ('inquirer', 'tech_collective', 'is_interested')
+
+        list_display = ('__str__', 'inquirer', 'is_interested')
+        list_filter = ('inquirer',)
+
+        class RestrictionsInlines(admin.TabularInline):
+            model = TechCollectiveInterest.restriction_scopes.through
+            fields = ('restriction', 'value')
+            readonly_fields = ('restriction', 'value')
+            extra = 0
+
+            def has_add_permission(self, request, obj):
+                # This should only display info, not allow changing
+                return False
+
+            def restriction(self, instance):
+                return instance.restrictionvalue.restriction.name
+            restriction.short_description = 'restriction name'
+
+            def value(self, instance):
+                return instance.restrictionvalue.value
+            value.short_description = 'local value'
+
+        inlines = [RestrictionsInlines]
+
+    admin.site.register(RestrictionValue)
     admin.site.register(InitiatedCollective)
     admin.site.register(CollectiveRSVP)
     admin.site.register(CollectiveApprovalResponse)
